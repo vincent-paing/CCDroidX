863d2154a2f7fc3c4361d982907a9cf4f6b8fc82
vincent-paing <dev.aungkyawpaing@gmail.com>
2022-04-24 17:49:19 +0700

    Warmup in app browser

--- app/src/main/java/dev/aungkyawpaing/ccdroidx/feature/MainActivity.kt
+++ app/src/main/java/dev/aungkyawpaing/ccdroidx/feature/MainActivity.kt
@@ -1,14 +1,16 @@
 package dev.aungkyawpaing.ccdroidx.feature
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsClient
+import androidx.browser.customtabs.CustomTabsServiceConnection
 import androidx.work.*
 import dagger.hilt.android.AndroidEntryPoint
 import dev.aungkyawpaing.ccdroidx.R
-import dev.aungkyawpaing.ccdroidx.databinding.ActivityMainBinding
 import dev.aungkyawpaing.ccdroidx.feature.sync.SyncProjectWorker
+import timber.log.Timber
 import java.time.Duration
-import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -17,6 +19,12 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setUpSyncWorker()
+    warmupBrowser()
+  }
+
+  private fun warmupBrowser() {
+    val browserPackageName = CustomTabsClient.getPackageName(this, null) ?: return
+    CustomTabsClient.connectAndInitialize(this, browserPackageName)
   }
 
   private fun setUpSyncWorker() {

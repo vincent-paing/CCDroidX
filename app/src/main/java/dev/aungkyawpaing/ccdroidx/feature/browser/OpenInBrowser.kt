bc88f2c1989a83e6b8671ff41fd304e9cefda7ed
vincent-paing <dev.aungkyawpaing@gmail.com>
2022-05-03 22:48:45 +0700

    Add settings to open link externally

--- app/src/main/java/dev/aungkyawpaing/ccdroidx/feature/browser/OpenInBrowser.kt
+++ app/src/main/java/dev/aungkyawpaing/ccdroidx/feature/browser/OpenInBrowser.kt
@@ -1,16 +1,33 @@
 package dev.aungkyawpaing.ccdroidx.feature.browser
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.net.toUri
+import androidx.preference.PreferenceManager
+import dev.aungkyawpaing.ccdroidx.R
+import dev.aungkyawpaing.ccdroidx.utils.extensions.showShortToast
+import dev.aungkyawpaing.ccdroidx.utils.intent.Intents
 import javax.inject.Inject
 
 class OpenInBrowser @Inject constructor() {
 
   fun openInBrowser(activity: Activity, url: String) {
-    val customTabsIntent = CustomTabsIntent.Builder().build();
-    customTabsIntent.launchUrl(activity, url.toUri())
-  }
+    val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity)
+    val doesLinkOpenExternally = sharedPreferences.getBoolean("others_browser", false)
 
+
+    if (doesLinkOpenExternally) {
+      try {
+        val viewUrlIntent = Intents.viewUrl(url)
+        activity.startActivity(viewUrlIntent)
+      } catch (exception: ActivityNotFoundException) {
+        activity.showShortToast(activity.getString(R.string.error_no_browser_installed))
+      }
+    } else {
+      val customTabsIntent = CustomTabsIntent.Builder().build()
+      customTabsIntent.launchUrl(activity, url.toUri())
+    }
+  }
 
 }

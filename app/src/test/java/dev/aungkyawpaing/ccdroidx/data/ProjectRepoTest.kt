ca11617449eb55ac39c69c05298a6867a1c35dcf
vincent-paing <dev.aungkyawpaing@gmail.com>
2022-06-27 22:33:17 +0700

    Add missing tests for ProjectRepo

--- app/src/test/java/dev/aungkyawpaing/ccdroidx/data/ProjectRepoTest.kt
+++ app/src/test/java/dev/aungkyawpaing/ccdroidx/data/ProjectRepoTest.kt
@@ -2,24 +2,23 @@ package dev.aungkyawpaing.ccdroidx.data
 
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 import dev.aungkyawpaing.ccdroidx.CCDroidXDb
-import dev.aungkyawpaing.ccdroidx.CoroutineTestRule
+import dev.aungkyawpaing.ccdroidx._testhelper_.CoroutineTest
 import dev.aungkyawpaing.ccdroidx._testhelper_.ProjectBuilder.buildProject
 import dev.aungkyawpaing.ccdroidx.api.FetchProject
+import dev.aungkyawpaing.ccdroidx.api.NetworkException
+import dev.aungkyawpaing.ccdroidx.api.ProjectResponse
 import dev.aungkyawpaing.ccdroidx.db.projectTableAdapter
+import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import org.junit.jupiter.api.*
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class ProjectRepoTest {
-
-  @get:Rule
-  var coroutineTestRule = CoroutineTestRule()
+class ProjectRepoTest : CoroutineTest() {
 
   private val fetchProject = mockk<FetchProject>()
   private val driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
@@ -27,29 +26,96 @@ class ProjectRepoTest {
   private val projectRepo = ProjectRepo(
     fetchProject,
     CCDroidXDb(driver, projectTableAdapter),
-    coroutineTestRule.testDispatcherProvider
+    testDispatcherProvider
   )
 
-  @Before
+  @BeforeEach
   fun setUp() {
     CCDroidXDb.Schema.create(driver)
   }
 
+  @Nested
+  @DisplayName("fetchRepo")
+  internal inner class FetchRepoFunction {
 
-  @Test
-  fun testSaveProject() = coroutineTestRule.scope.runTest {
-    val project = buildProject(id = -1L)
+    @Test
+    fun `invoke fetch and map response`() = runTest {
+      val url = "url"
 
-    projectRepo.saveProject(project)
+      coEvery {
+        fetchProject.requestForProjectList(url)
+      } returns listOf(
+        ProjectResponse(
+          name = "Project Name",
+          activity = BuildState.SLEEPING,
+          lastBuildStatus = BuildStatus.SUCCESS,
+          lastBuildLabel = "Last Build Label",
+          lastBuildTime = ZonedDateTime.of(2022, 4, 21, 0, 0, 0, 0, ZoneId.of("UTC")),
+          nextBuildTime = ZonedDateTime.of(2022, 4, 22, 0, 0, 0, 0, ZoneId.of("UTC")),
+          webUrl = "https://example.com/master",
+          feedUrl = "https://www.example.com/cc.xml",
+        )
+      )
 
-    val savedProjects = projectRepo.getAll().first()
-    Assert.assertEquals(listOf(project.copy(id = 1)), savedProjects)
+      val expected = listOf(
+        Project(
+          id = -1L,
+          name = "Project Name",
+          activity = BuildState.SLEEPING,
+          lastBuildStatus = BuildStatus.SUCCESS,
+          lastBuildLabel = "Last Build Label",
+          lastBuildTime = ZonedDateTime.of(2022, 4, 21, 0, 0, 0, 0, ZoneId.of("UTC")),
+          nextBuildTime = ZonedDateTime.of(2022, 4, 22, 0, 0, 0, 0, ZoneId.of("UTC")),
+          webUrl = "https://example.com/master",
+          feedUrl = "https://www.example.com/cc.xml",
+          isMuted = false,
+          mutedUntil = null
+        )
+      )
 
+      val actual = projectRepo.fetchRepo(url)
 
-    val updatedProject = savedProjects[0].copy(name = "updated project")
-    projectRepo.saveProject(updatedProject)
+      Assertions.assertEquals(expected, actual)
+    }
 
-    Assert.assertEquals(listOf(updatedProject), projectRepo.getAll().first())
+    @Test
+    fun `pipe exception thrown from fetch repo`() = runTest {
+      val execption = NetworkException()
+      coEvery {
+        fetchProject.requestForProjectList(any())
+      } throws execption
 
+      val result = kotlin.runCatching {
+        projectRepo.fetchRepo("url")
+      }.onFailure { actual ->
+        Assertions.assertEquals(execption, actual)
+      }
+
+      Assertions.assertTrue(result.isFailure)
+    }
+  }
+
+  @Nested
+  @DisplayName("saveProject")
+  internal inner class SaveProject {
+
+    @Test
+    fun `insert if id is -1L`() = runTest {
+      val project = buildProject(id = -1L)
+
+      projectRepo.saveProject(project)
+
+      Assertions.assertEquals(listOf(project.copy(id = 1)), projectRepo.getAll().first())
+    }
+
+    @Test
+    fun `update if id is not -1L`() = runTest {
+      val project = buildProject(id = -1L)
+      projectRepo.saveProject(project)
+
+      val updateProject = project.copy(id = 1, name = "Updated name")
+      projectRepo.saveProject(updateProject)
+      Assertions.assertEquals(listOf(updateProject), projectRepo.getAll().first())
+    }
   }
 }

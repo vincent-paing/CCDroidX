c97fd34ecee192564db6bf6164da2e6583a39846
vincent-paing <dev.aungkyawpaing@gmail.com>
2022-06-29 22:29:01 +0700

    Use basic auth if username and passoword is given

--- app/src/main/java/dev/aungkyawpaing/ccdroidx/api/FetchProject.kt
+++ app/src/main/java/dev/aungkyawpaing/ccdroidx/api/FetchProject.kt
@@ -1,5 +1,6 @@
 package dev.aungkyawpaing.ccdroidx.api
 
+import okhttp3.Credentials
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import timber.log.Timber
@@ -9,10 +10,19 @@ class FetchProject @Inject constructor(
   private val client: OkHttpClient
 ) {
 
-  fun requestForProjectList(url: String, username: String? = null, password: String? = null): List<ProjectResponse> {
+  fun requestForProjectList(
+    url: String,
+    username: String? = null,
+    password: String? = null
+  ): List<ProjectResponse> {
     val request = try {
       Request.Builder()
         .url(url)
+        .also {
+          if (username != null && password != null) {
+            it.addHeader("Authorization", Credentials.basic(username, password))
+          }
+        }
         .build()
     } catch (exception: IllegalArgumentException) {
       Timber.e(exception)
@@ -21,7 +31,7 @@ class FetchProject @Inject constructor(
 
     client.newCall(request).executeOrThrow().use { response ->
       try {
-        return CCTrayParser.parseResponse(response ?: throw NetworkException())
+        return CCTrayParser.parseResponse(response)
       } catch (exception: Exception) {
         Timber.e(exception)
         throw UnsupportedServerException()
--- app/src/test/java/dev/aungkyawpaing/ccdroidx/api/FetchProjectTest.kt
+++ app/src/test/java/dev/aungkyawpaing/ccdroidx/api/FetchProjectTest.kt
@@ -1,28 +1,115 @@
 package dev.aungkyawpaing.ccdroidx.api
 
-import dev.aungkyawpaing.ccdroidx.CoroutineTestRule
+import dev.aungkyawpaing.ccdroidx._testhelper_.CoroutineTest
 import dev.aungkyawpaing.ccdroidx.data.BuildState
 import dev.aungkyawpaing.ccdroidx.data.BuildStatus
-import dev.aungkyawpaing.ccdroidx.data.Project
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import okhttp3.Credentials
 import okhttp3.OkHttpClient
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
-import org.junit.Assert
-import org.junit.Rule
-import org.junit.Test
-import java.time.*
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
 
 
 @ExperimentalCoroutinesApi
-class FetchProjectTest {
-
-  @get:Rule
-  var coroutineTestRule = CoroutineTestRule()
+class FetchProjectTest : CoroutineTest() {
 
   @Test
-  fun testParseResponseCorrectly() = runTest {
+  fun `makes request with url`() = runTest {
+    val server = MockWebServer()
+
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(200)
+    )
+
+    server.start()
+    val fetchProject = FetchProject(OkHttpClient())
+
+    runCatching {
+      fetchProject.requestForProjectList(server.url("/cc.xml").toString(), null, null)
+    }
+
+    val request1 = server.takeRequest()
+    Assertions.assertEquals("/cc.xml", request1.path)
+
+    server.shutdown()
+  }
+
+  @Test
+  fun `make request with no auth if username is null`() = runTest {
+    val server = MockWebServer()
+
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(200)
+    )
+
+    server.start()
+    val fetchProject = FetchProject(OkHttpClient())
+
+    runCatching {
+      fetchProject.requestForProjectList(server.url("/cc.xml").toString(), null, "password")
+    }
+
+    val request1 = server.takeRequest()
+    Assertions.assertNull(request1.getHeader("Authorization"))
+
+    server.shutdown()
+  }
+
+  @Test
+  fun `make request with no auth if password is null`() = runTest {
+    val server = MockWebServer()
+
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(200)
+    )
+
+    server.start()
+    val fetchProject = FetchProject(OkHttpClient())
+
+    runCatching {
+      fetchProject.requestForProjectList(server.url("/cc.xml").toString(), "username", null)
+    }
+
+    val request1 = server.takeRequest()
+    Assertions.assertNull(request1.getHeader("Authorization"))
+
+    server.shutdown()
+  }
+
+  @Test
+  fun `make request with auth if there is username and password`() = runTest {
+    val server = MockWebServer()
+
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(200)
+    )
+
+    server.start()
+    val fetchProject = FetchProject(OkHttpClient())
+
+    runCatching {
+      fetchProject.requestForProjectList(server.url("/cc.xml").toString(), "username", "password")
+    }
+
+    val request1 = server.takeRequest()
+    val credential = Credentials.basic("username", "password")
+    Assertions.assertEquals(credential, request1.getHeader("Authorization"))
+
+    server.shutdown()
+  }
+
+  @Test
+  fun `parse response Correctly`() = runTest {
     val server = MockWebServer()
 
     server.enqueue(
@@ -66,11 +153,7 @@ class FetchProjectTest {
       )
     )
 
-    Assert.assertEquals(expected, actual)
-
-    val request1 = server.takeRequest()
-    Assert.assertEquals("/", request1.path)
-
-    server.shutdown()
+    Assertions.assertEquals(expected, actual)
   }
+
 }

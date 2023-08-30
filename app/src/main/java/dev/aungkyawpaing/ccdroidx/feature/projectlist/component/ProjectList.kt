5eb23c9cfb1014f02f696af72eac2df4abde4a8d
vincent-paing <dev.aungkyawpaing@gmail.com>
2023-08-30 07:05:50 +0700

    Use Adaptive grid layout to support multi form factor

--- app/src/main/java/dev/aungkyawpaing/ccdroidx/feature/projectlist/component/ProjectList.kt
+++ app/src/main/java/dev/aungkyawpaing/ccdroidx/feature/projectlist/component/ProjectList.kt
@@ -3,8 +3,9 @@ package dev.aungkyawpaing.ccdroidx.feature.projectlist.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -18,11 +19,12 @@ fun ProjectList(
   onToggleMute: ((project: Project) -> Unit),
   modifier: Modifier = Modifier
 ) {
-
-  LazyColumn(
-    contentPadding = PaddingValues(8.dp),
+  LazyVerticalGrid(
+    contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
     verticalArrangement = Arrangement.spacedBy(8.dp),
-    modifier = modifier.padding(bottom = 56.dp)
+    horizontalArrangement = Arrangement.spacedBy(16.dp),
+    columns = GridCells.Adaptive(minSize = 300.dp),
+    modifier = modifier.padding(bottom = 56.dp),
   ) {
     itemsIndexed(
       items = projectList,
